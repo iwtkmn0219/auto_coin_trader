2208610c7b8e5ecf20331a85d5e8e0a58e7d5cbf
iwtkmn0219 <iwtkmn0219@naver.com>
2023-08-03 10:28:50 +0900

    💄 전반적인 UI수정

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ print("로그인 하였습니다.")
 
 def login_upbit() -> pyupbit.Upbit:
     # 로그인 정보 입력
-    access = input()
-    secret = input()
+    access = input("Access key: ")
+    secret = input("Secret key: ")
     return pyupbit.Upbit(access, secret)
 
 # 프로그램 시작
 print("==========================")
 print("Welcome! Auto Coin Trader!")
 print("==========================")
-login_upbit()
+# 로그인
+my_upbit = login_upbit()
 print("\033[32m===== Login  Success =====\033[0m")
+
 today_coin_list = []
 while True:
     try:
@@ -31,34 +33,27 @@ while True:
         start_time = pyupbit.get_ohlcv("KRW-BTC", interval="day", count=1).index[0]
         end_time = start_time + datetime.timedelta(days=1)
         
-        # 하루에 한 번 리스트를 불러온다. 임의의 확률로 리스트가 비어있는 경우도 확인한다.
-        if start_time + datetime.timedelta(seconds=60) < now < start_time + datetime.timedelta(seconds=300) or len(today_coin_list) == 0:
+        # 9:00~9:02 리스트를 불러온다. 임의의 확률로 리스트가 비어있는 경우도 확인한다.
+        if start_time < now < start_time + datetime.timedelta(minutes=2) or len(today_coin_list) == 0:
             # 우선 리스트를 비운다.
             today_coin_list.clear()
             
-            print("\033[31m코인의 리스트를 불러옵니다.(약30초 소요)\033[0m")
             # 코인 정보 리스트를 불러온다.
+            print("\033[31mGet Coins list\033[0m(at 30 seconds..)")
             today_coin_list = functions.calculate_all_target_price(N)
-            print(f"\033[31m{N}개의 코인에 대한 정보를 불러왔습니다.\033[0m")
+            print(f"mGet \033[31{N}\033[0m coins information")
             
-            pprint.pprint(today_coin_list)
-            
-            print("\033[31mADD INFORMATION AND PREDICT CLOSE PRICE\033[0m")
-            # 코인별 거래 확인용 체크란 생성
+            # 코인별 예측값 생성
+            print("\033[31mAdd additional information(predicted close price..)\033[0m")
             for i in range(len(today_coin_list)):
                 predicted_close_price = functions.calculate_predict_close_price(today_coin_list[i][0])
-                print(predicted_close_price)
-                
                 today_coin_list[i].append(predicted_close_price)
                 today_coin_list[i].append(0)
             print("\033[31mDONE\033[0m")
-                
             pprint.pprint(today_coin_list)
         
-        # # 금일 09:10 ~ 명일 08:50 에만 거래 활성화
-        # if start_time + datetime.timedelta(seconds=600) < now < end_time - datetime.timedelta(seconds=600):
-        # 금일 09:00 ~ 금일 24:00 에만 거래 활성화
-        if start_time < now < start_time + datetime.timedelta(hours=15):
+        # 금일 09:02 ~ 명일 08:55 에만 거래 활성화
+        if start_time + datetime.timedelta(minutes=2) < now < end_time - datetime.timedelta(minutes=5):
             # 코인별 정보 확인
             for i, e in enumerate(today_coin_list):
                 # 코인별 정보 파싱
@@ -84,29 +79,26 @@ while True:
                     if current_price > target_price and current_price < predicted_close_price:
                         my_krw = my_upbit.get_balance("KRW")
                         
-                        print(f"\033[31m매수가 도달\033[0m: \033[35m{market_code}\033[0m")
+                        print(f"\033[31mBuyout Reached\033[0m: \033[35m{market_code}\033[0m")
                         # 업비트 최소 거래 금액
                         if my_krw > MONEY:
                             # 매수 진행
                             my_upbit.buy_market_order(market_code, MONEY)
                             today_coin_list[i][4] = 1
-                            print(f"체결: \033[35m{market_code}\033[0m")
+                            print(f"Sign: \033[35m{market_code}\033[0m")
                         else:
                             print("\033[41m-=-거래금액 불충족-=-\033[0m")
                     # else:
                     #     print(f"\033[34m매수가 미만\033[0m: \033[35m{market_code}\033[0m")
                 # else:
                 #     print(f"\033[35m{market_code}\033[0m는 이미 체결된 코인입니다.")
-                time.sleep(1)
         # 지정 시간 이탈 시 전량 매도 후 프로그램 종료
         else:
-            print("시장 종료")
+            print("Market Finished")
             for i, e in enumerate(today_coin_list):
                 market_code = e[0]
                 balance = my_upbit.get_balance(market_code)
                 my_upbit.sell_market_order(market_code, balance)
-            print("프로그램을 종료합니다")
-            break
         
         time.sleep(1)
         
