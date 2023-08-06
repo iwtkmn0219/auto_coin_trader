d13274c34da7110a12a46d7dc0a1bf7cdd7d3f2b
iwtkmn0219 <iwtkmn0219@naver.com>
2023-08-06 11:39:35 +0900

    ✨ 하루에 한 번 보유 잔고를 디스코드로 보내주는 기능

--- discord_notifier.py
+++ discord_notifier.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
-from datetime import datetime, time
+import pyupbit
+from datetime import datetime, timedelta
 
 # discord bot token
 DISCORD_TOKEN = (
@@ -8,6 +9,14 @@ DISCORD_TOKEN = (
 )
 # discord channel id (일반)
 CHANNEL_ID = 1137546202708185182
+
+# 업비트 로그인
+f = open("./tmp.txt")
+lines = f.readlines()
+access = lines[0].strip()
+secret = lines[1].strip()
+f.close()
+my_upbit = pyupbit.Upbit(access, secret)
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -22,3 +31,29 @@ async def send_message(msg: str):
         await channel.send(msg)
     else:
         print("Cannot found channel")
+
+
+# 특정 시간에 메시지를 전송한다.
+async def send_message_at():
+    current_time = datetime.now()
+    if current_time.hour == 7 and current_time.minute == 58:
+        current_krw = my_upbit.get_balance("KRW")
+        task = asyncio.create_task(send_message(f"{current_time.year}년 {current_time.month}월 {current_time.day}일: 현재 보유 KRW는 {format(round(current_krw), ',')}원 입니다."))
+        await task
+    await asyncio.sleep(59)
+
+
+@client.event
+async def on_ready():
+    print(f"Success Login (로그인 정보: {client.user})")
+    while True:
+        task = asyncio.create_task(send_message_at())
+        await task
+
+
+@client.event
+async def on_diconnect():
+    print(f"{client.user}의 연결이 끊어졌습니다.")
+
+
+client.run(DISCORD_TOKEN)
