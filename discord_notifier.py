6ae4cb97043f4479d9272d373c2f75e68b426b9e
iwtkmn0219 <iwtkmn0219@naver.com>
2023-08-06 10:47:06 +0900

    ✨ 메시지 보내기 기능

--- discord_notifier.py
+++ discord_notifier.py
@@ -3,10 +3,22 @@ import asyncio
 from datetime import datetime, time
 
 # discord bot token
-DISCORD_TOKEN = "MTEzNzU0MDI4OTg5NzE3NzEzOA.GffzZO.VTtO95OlDvAKLHlMedCXKvhsh9GNMGJMZ2Ju0g"
+DISCORD_TOKEN = (
+    "MTEzNzU0MDI4OTg5NzE3NzEzOA.GffzZO.VTtO95OlDvAKLHlMedCXKvhsh9GNMGJMZ2Ju0g"
+)
+# discord channel id (일반)
+CHANNEL_ID = 1137546202708185182
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
 client = discord.Client(intents=intents)
+
+
+async def send_message(msg: str):
+    channel = client.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send(msg)
+    else:
+        print("Cannot found channel")
